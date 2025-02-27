11587095efd7de6b72d7b688dcf76a6914957390
yan <doutianmianxia@qq.com>
2025-02-28 01:37:30 +0800

    (modulebuild): 重构 Maven 构建配置 - 使用了maven指定启动环境 spring.profiles.active: @spring.profiles.active@ - 更新了多个模块的 pom.xml 文件结构 - 调整了依赖管理和属性定义 - 移除了冗余的构建配置 - 统一了 Spring Boot 和其他依赖的版本

--- micro-service-module-all/im/src/main/java/com/minimalism/ImApplication.java
+++ micro-service-module-all/im/src/main/java/com/minimalism/ImApplication.java
@@ -1,41 +1,12 @@
 package com.minimalism;
 
-//import com.minimalism.im.config.CommonConfig;
-import com.minimalism.scan.SwaggerScan;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
 
 @SpringBootApplication
-//@Import({CommonConfig.class, SwaggerScan.class})
-//@ComponentScan(basePackages = {"com.parent.common.log","com.parent.common.exception"})
-//@ComponentScan(excludeFilters = {
-//        @ComponentScan.Filter(type = FilterType.REGEX,pattern = "com.yan.filter.CorsFilter")
-//})
 public class ImApplication /*implements CommandLineRunner*/ {
-    /**
-     * im 后端模板建立
-     * @param args
-     */
-    //@Resource
-    //private CoordinationNettyServer nettyServer;
 
     public static void main(String[] args) {
         SpringApplication.run(ImApplication.class, args);
     }
-
-
-/*    @Override
-    public void run(String... args) throws Exception {
-        CompletableFuture.runAsync(()->{
-            try {
-                //nettyServer.start();
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        });
-    }*/
 }
