41f142954b66f08b95bf11f6976c9110c9274cf7
yan <doutianmianxia@qq.com>
2025-03-16 01:57:05 +0800

    feat(AliyunMailUtils): 添加邮件发送功能及测试用例

--- common-base-all/common-code/src/main/java/com/minimalism/utils/mail/AliyunMailUtils.java
+++ common-base-all/common-code/src/main/java/com/minimalism/utils/mail/AliyunMailUtils.java
@@ -1,5 +1,6 @@
 package com.minimalism.utils.mail;
 
+import cn.hutool.core.collection.CollUtil;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dm.model.v20151123.SingleSendMailRequest;
@@ -15,6 +16,7 @@ import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,9 +38,9 @@ public class AliyunMailUtils {
     private static final String FROM_ALIAS = "发信人别名";
     private static final String TAG_NAME = "邮件标签";
 
-    @SuperBuilder
     @Data
     @Accessors(chain = true)
+    @SuperBuilder
     @NoArgsConstructor
     @AllArgsConstructor
     static class MailClient {
@@ -69,6 +71,10 @@ public class AliyunMailUtils {
          * 邮件内容
          */
         private String content;
+
+        boolean sendMail() {
+            return AliyunMailUtils.sendMail(this);
+        }
     }
 
     /**
@@ -105,7 +111,7 @@ public class AliyunMailUtils {
         SingleSendMailRequest request = new SingleSendMailRequest();
         request.setAccountName(accountName);//控制台创建的发信地址
         request.setFromAlias(senderName);//发信人昵称
-        request.setAddressType(1);
+        request.setAddressType(1);// 1 表示发信地址
         request.setReplyToAddress(true);
         request.setTagName(tagName);
         request.setToAddress(address);
@@ -137,6 +143,37 @@ public class AliyunMailUtils {
         String tagName = mailClient.getTagName();
         List<String> toAddressList = mailClient.getToAddressList();
         return sendMail(iAcsClient, toAddressList, subject, sendAddress, senderName, tagName, content);
+    }
+
+
+    public static void main(String[] args) {
+
+        String accessKeyId = "accessKeyId";
+        String accessKeySecret = "accessKeySecret";
+        String endpoint = "cn-hangzhou";
+        String tagName = "1";
+        String content = "测试";
+        String subject = "测试主题";
+
+
+        ArrayList<String> toAddressList = CollUtil.newArrayList("xxx@qq.com");
+        String sendAddress = "your_sender@example.com";
+
+        String senderName = "测试";
+
+        boolean sendMail = false;
+        MailClient build = MailClient.builder()
+                .iAcsClient(AliyunMailUtils.createIAcsClient(accessKeyId, accessKeySecret, endpoint))
+                .content(content)
+                .subject(subject)
+                .tagName(tagName)
+                .toAddressList(toAddressList)
+                .sendAddress(sendAddress)
+                .senderName(senderName)
+                .build();
+        sendMail = build.sendMail();
+        // sendMail = AliyunMailUtils.sendMail(build);
+        System.err.println("sendMail:" + sendMail);
     }
 
 }
