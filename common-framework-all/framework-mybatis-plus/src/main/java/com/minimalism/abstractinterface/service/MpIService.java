a72d9fc7abfc0a1ffa5e3ea60e5de2c321669c4c
yan <doutianmianxia@qq.com>
2025-04-25 23:04:52 +0800

    feat(transaction): 添加本地事务转全局事务支持
    - 新增 CustomSeataProperties 配置类，用于全局配置事务转换
    - 添加 CustomTransactional 注解，用于自定义事务管理
    - 实现 ToGlobalTransactionalAspect 切面，处理事务转换逻辑
    - 更新 MpIService 接口，为常用方法添加事务支持
    - 新增 ToGlobal 注解，用于标记需要转换为全局事务的方法

--- common-framework-all/framework-mybatis-plus/src/main/java/com/minimalism/abstractinterface/service/MpIService.java
+++ common-framework-all/framework-mybatis-plus/src/main/java/com/minimalism/abstractinterface/service/MpIService.java
@@ -1,6 +1,11 @@
 package com.minimalism.abstractinterface.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.Map;
 
 /**
  * @Author yan
@@ -8,4 +13,39 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @Description
  */
 public interface MpIService<T> extends IService<T> {
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean save(T entity) {
+        return IService.super.save(entity);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean removeById(Serializable id) {
+        return IService.super.removeById(id);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean removeById(Serializable id, boolean useFill) {
+        return IService.super.removeById(id, useFill);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean removeById(T entity) {
+        return IService.super.removeById(entity);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean removeByMap(Map<String, Object> columnMap) {
+        return IService.super.removeByMap(columnMap);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    default boolean remove(Wrapper<T> queryWrapper) {
+        return IService.super.remove(queryWrapper);
+    }
 }
