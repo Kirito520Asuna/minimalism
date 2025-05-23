433a25cee0e33ef2496e50ef2bea8beeaae57344
yan <doutianmianxia@qq.com>
2025-05-23 20:07:42 +0800

    fix: 修复RedisBanAspect请求上下文为空时放行问题

--- common-framework-all/framework-redis/src/main/java/com/minimalism/redis/aop/aspect/RedisBanAspect.java
+++ common-framework-all/framework-redis/src/main/java/com/minimalism/redis/aop/aspect/RedisBanAspect.java
@@ -14,6 +14,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -59,7 +60,13 @@ public class RedisBanAspect implements AbsRedisAspect {
     @Around("execution(* com..*Controller.*(..))")
     public Object checkGlobalBan(ProceedingJoinPoint joinPoint) throws Throwable {
         //log.debug("[{}]执行全局检查", System.currentTimeMillis());
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            log.debug("没有请求上下文，放行");
+            // 如果没有请求上下文，则直接放行
+            return joinPoint.proceed();
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
         String ipAddress = getClientIP(request);
         BanConfiguration banConfiguration = DEFAULT_BAN_CONFIGURATION;
         boolean globalBanEnabled = banConfiguration.isGlobalBanEnabled();
