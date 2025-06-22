e4a30b3f4a54a42f87f0c8f56391900be1532181
yan <doutianmianxia@qq.com>
2025-06-23 00:52:57 +0800

    feat(framework-sharding-sphere): 添加分库分表ShardingSphere模块 (springboot3 迁移 springboot2)

---
+++ micro-framework/framework-sharding-sphere/src/main/java/com/actual/combat/sharding_sphere/dynamic/Impl/ShardingSphereDynamicDataSourceImpl.java
@@ -0,0 +1,13 @@
+package com.actual.combat.sharding_sphere.dynamic.Impl;
+
+import com.actual.combat.dynamic_datasource.abs.AbsDynamicDataSource;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author yan
+ * @Date 2024/10/25 上午9:21:31
+ * @Description
+ */
+@Service
+public class ShardingSphereDynamicDataSourceImpl implements AbsDynamicDataSource {
+}
